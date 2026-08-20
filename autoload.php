e9cfc5bc53587584c652161077934c40394cb44d
bulatik205 <bulatmullagal@gmail.com>
2026-08-20 18:26:23 +0400

    update: added autoload registrator

---
+++ autoload.php
@@ -0,0 +1,8 @@
+<?php
+spl_autoload_register(function ($className) {
+    $file = __DIR__ . '/src/' . str_replace('\\', '/', $className) . '.php';
+    
+    if (file_exists($file)) {
+        require $file;
+    }
+});
