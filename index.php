d4772f77abe319499a6f494819887f6479da06a5
bulatik205 <bulatmullagal@gmail.com>
2026-08-20 18:24:57 +0400

    update: added base html marldown

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once("autoload.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Food Controller</title>
+    <link rel="stylesheet" href="static/css/index/index.css">
+</head>
+<body>
+    <div class="main">
+        <div class="main--header">
+            <img src="static/images/food_header.jpg" alt="">
+        </div>
+
+        <div class="main--text">
+            <h1>Привет!</h1>
+            <hr>
+            <p>Контроль питания учащихся в одном месте</p>
+        </div>
+
+        <div class="main--crd">
+            <div>Удобное управление</div>
+            <div>Аналитика</div>
+            <div>Доступы UToken</div>
+            <div>CRD операции</div>
+        </div>
+
+        <div class="main--start">
+            <button><a href="">Я ученик</a></button>
+            <button><a href="">Я админ</a></button>
+        </div>
+    </div>
+</body>
+</html>
